7e5c4c7af94061b32f142ea233588aa3c6b38ba8
Kong To <newlight77@gmail.com>
2022-05-17 22:20:35 +0200

    feature: add components

---
+++ components/footer/footer.component.tsx
@@ -0,0 +1,26 @@
+import React, { FunctionComponent } from 'react';
+
+import styles from './footer.module.css';
+
+const pjson = require('../../package.json')
+
+const Footer: FunctionComponent = () => {
+  const year = new Date().getFullYear()
+  return (
+    <footer className={styles.footer}>
+      <span className={styles.devTeam}>
+        Created with <span className={styles.devTeam__love}> ♥ </span> by{' '}
+        <a
+          className={styles.link}
+          href="https://www.techhive.io"
+          target="_blank"
+        >
+          TechHive.IO
+        </a>
+        {' ' + year} - Version {pjson.version}
+      </span>
+    </footer>
+  )
+}
+
+export default Footer
