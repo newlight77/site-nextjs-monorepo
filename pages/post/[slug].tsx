51812961c9a428c412ac33906d5d45547248cbf6
Kong To <newlight77@gmail.com>
2023-01-30 21:36:51 +0100

    styles: refactoring SyntaxHighlighter for markdown code

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -7,33 +7,13 @@ import { BlogPost } from '../../models/blog.post';
 import { MetaTags, PageType, RobotsContent } from '../../models/tags';
 import { ContentfulService } from '../../service/contentful.service';
 
-import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter'
-import tsx from 'react-syntax-highlighter/dist/cjs/languages/prism/tsx'
-import typescript from 'react-syntax-highlighter/dist/cjs/languages/prism/typescript'
-import scss from 'react-syntax-highlighter/dist/cjs/languages/prism/scss'
-import bash from 'react-syntax-highlighter/dist/cjs/languages/prism/bash'
-import markdown from 'react-syntax-highlighter/dist/cjs/languages/prism/markdown'
-import json from 'react-syntax-highlighter/dist/cjs/languages/prism/json'
-import { oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
-import rangeParser from 'parse-numeric-range'
+import MarkdownComponents from '../../components/markdown/markdown-syntax-highlighter';
 
-SyntaxHighlighter.registerLanguage('tsx', tsx)
-SyntaxHighlighter.registerLanguage('typescript', typescript)
-SyntaxHighlighter.registerLanguage('scss', scss)
-SyntaxHighlighter.registerLanguage('bash', bash)
-SyntaxHighlighter.registerLanguage('markdown', markdown)
-SyntaxHighlighter.registerLanguage('json', json)
 
 type Props = {
   article: BlogPost;
   suggestedArticles: BlogPost[];
 };
-
-type CodeBlock = {
-  node: any,
-  inline: any,
-  className: string
-}
 
 const renderCards = (suggestions: any) =>
   suggestions.map((suggestion: any, index: number) => (
@@ -51,49 +31,6 @@ const PostPage: NextPage<Props, any> = (props: Props) => {
     type: PageType.article
   };
 
-  const syntaxTheme = oneDark;
-
-  const MarkdownComponents: object = {
-    code({ node, inline, className, ...props }: CodeBlock) {
-      const match = /language-(\w+)/.exec(className || '')
-      const hasMeta = node?.data?.meta
-
-      const applyHighlights: object = (applyHighlights: number) => {
-        if (hasMeta) {
-          const RE = /{([\d,-]+)}/
-          const metadata = node.data.meta?.replace(/\s/g, '')
-          const strlineNumbers = RE?.test(metadata)
-            ? RE?.exec(metadata)![1]
-            : '0'
-          const highlightLines = rangeParser(strlineNumbers)
-          const highlight = highlightLines
-          const data: string | null = highlight.includes(applyHighlights)
-            ? 'highlight'
-            : null
-          return { data }
-        } else {
-          return {}
-        }
-      }
-
-      return match ? (
-        <SyntaxHighlighter
-          style={syntaxTheme}
-          language={match[1]}
-          PreTag="div"
-          className="codeStyle"
-          showLineNumbers={true}
-          wrapLines={hasMeta ? true : false}
-          useInlineStyles={true}
-          lineProps={applyHighlights}
-          children={node.children}
-          {...props}
-        />
-      ) : (
-        <code className={className} {...props} />
-      )
-    },
-  }
 
   return (
     <Layout metaTags={postMetaTags}>
