d0ebddbdacc6ee2c6c37cb7a211737356e161930
Kong To <newlight77@gmail.com>
2023-03-16 08:50:16 +0100

    config: add more next config with bundle analyzer and rewrites

--- next.config.js
+++ next.config.js
@@ -1,21 +1,55 @@
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
 
-const next_config = {
+const config_webpack = {
   webpack: config => {
     config.plugins = config.plugins || [];
-
     config.plugins = [
       ...config.plugins,
-      // Read the .env file
       new Dotenv({
         path: path.join(__dirname, '.env'),
         systemvars: true
       })
     ];
-
     return config;
   },
-}
+};
 
-module.exports = {...next_config};
+const config_rewrites = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/notion/:path*',
+        destination: 'https://api.notion.com/v1/:path*',
+      },
+    ];
+  },
+};
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const config_static_assets = {
+    staticPageGenerationTimeout: 300,
+    images: {
+      domains: [
+        'www.notion.so',
+        'notion.so',
+        'images.unsplash.com',
+        'pbs.twimg.com',
+        'abs.twimg.com',
+        's3.us-west-2.amazonaws.com',
+        'transitivebullsh.it'
+      ],
+      formats: ['image/avif', 'image/webp'],
+      dangerouslyAllowSVG: true,
+      contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
+    }
+};
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+
+
+module.exports = withBundleAnalyzer(
+  {...config_webpack, ...config_rewrites, ...config_static_assets}
+)
