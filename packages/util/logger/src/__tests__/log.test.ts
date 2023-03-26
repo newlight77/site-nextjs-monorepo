45a72d94d83d67f9917b5a2e3502fe39f0ac754f
Kong To <newlight77@gmail.com>
2023-03-26 22:13:14 +0200

    enhance logger by creating a class

--- packages/util/logger/src/__tests__/log.test.ts
+++ packages/util/logger/src/__tests__/log.test.ts
@@ -1,10 +1,10 @@
-import { log } from "..";
+import { logger } from "..";
 
 jest.spyOn(global.console, "log");
 
 describe("logger", () => {
   it("prints a message", () => {
-    log("hello");
+    logger.log("hello");
     expect(console.log).toBeCalled();
   });
 });
--- packages/util/logger/src/index.ts
+++ packages/util/logger/src/index.ts
@@ -1,3 +1,19 @@
-export const log = (str: any) => {
-  console.log("logger: " + str);
-};
+
+class Logger {
+  log = (message?: any, ...optionalParams: any[]) => {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const colorfulParams = require('util').inspect(optionalParams, { colors: true, depth: 5 })
+      console.log(`------    NotionService  ${message}`, colorfulParams);
+  };
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    log_ = (message?: any, ...optionalParams: any[]) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const colorfulParams = optionalParams;
+  };
+
+  error = (error: unknown, ...optionalParams: any[]) => this.log(error, optionalParams);
+}
+
+
+export const logger = new Logger();
