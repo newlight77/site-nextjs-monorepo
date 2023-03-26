82e50fc80ab020f69c0fd5d3318a6ad97e3c57b5
Kong To <newlight77@gmail.com>
2023-03-26 16:28:42 +0200

    enhance eslint packages

--- packages/config/eslint-config-custom/index.js
+++ packages/config/eslint-config-custom/index.js
@@ -5,9 +5,6 @@ module.exports = {
   },
   extends: [
     "next",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
     "prettier",
     "turbo",
   ],
@@ -22,12 +19,17 @@ module.exports = {
       presets: [require.resolve("next/babel")],
     },
   },
-  plugins: ["@typescript-eslint", "react", "react-hooks"],
+  plugins: ["@typescript-eslint", "react"],
   rules: {
+    "@next/next/no-css-tag": "off",
     "@next/next/no-html-link-for-pages": "off",
+    "@next/next/no-img-element": "off",
     "@typescript-eslint/no-explicit-any": 0,
     "@typescript-eslint/no-non-null-assertion": 0,
     "@typescript-eslint/no-unused-vars": 2,
-    "react/prop-types": 0
+    "import/no-anonymous-default-export": 0,
+    "react/prop-types": 0,
+    "react-hooks/exhaustive-deps": 0,
+    "turbo/no-undeclared-env-vars": 0
   },
 };
