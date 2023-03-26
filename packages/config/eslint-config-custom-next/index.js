03572db2cb559c0aad6d0c1d69931dca3f21b927
Kong To <newlight77@gmail.com>
2023-03-26 16:49:28 +0200

    defined specific eslint-config-custom for simple ts, react, next

--- packages/config/eslint-config-custom-next/index.js
+++ packages/config/eslint-config-custom-next/index.js
@@ -4,18 +4,35 @@ module.exports = {
     "node": true
   },
   extends: [
+    "next",
     "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
     "prettier",
     "turbo",
   ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve("next/babel")],
+    },
+  },
+  plugins: ["@typescript-eslint", "react"],
   rules: {
+    "@next/next/no-css-tag": "off",
     "@next/next/no-html-link-for-pages": "off",
+    "@next/next/no-img-element": "off",
     "@typescript-eslint/no-explicit-any": 0,
     "@typescript-eslint/no-non-null-assertion": 0,
     "@typescript-eslint/no-unused-vars": 2,
     "import/no-anonymous-default-export": 0,
+    "react/prop-types": 0,
+    "react-hooks/exhaustive-deps": 0,
     "turbo/no-undeclared-env-vars": 0
   },
 };
--- packages/config/eslint-config-custom-react/index.js
+++ packages/config/eslint-config-custom-react/index.js
@@ -5,17 +5,25 @@ module.exports = {
   },
   extends: [
     "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
     "prettier",
     "turbo",
   ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["react", "react-hooks"],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
     "@typescript-eslint/no-explicit-any": 0,
     "@typescript-eslint/no-non-null-assertion": 0,
     "@typescript-eslint/no-unused-vars": 2,
     "import/no-anonymous-default-export": 0,
+    "react/prop-types": 0,
+    "react-hooks/exhaustive-deps": 0,
     "turbo/no-undeclared-env-vars": 0
   },
 };
