86196c2918e523943fddcfe4fade6ad16e948841
Kong To <newlight77@gmail.com>
2023-04-02 11:21:55 +0200

    update Input and CounterButton components

--- packages/ui/react-library/src/atoms/counter-button/CounterButton.tsx
+++ packages/ui/react-library/src/atoms/counter-button/CounterButton.tsx
@@ -1,20 +1,32 @@
 import * as React from "react";
 import './CounterButton.module.css';
 
-export const CounterButton: React.FC = () => {
+type PropsButton = {
+  className?: string;
+  id?: string;
+  type?: 'button' | 'submit' | 'reset' | undefined;
+  label?: string;
+  children?: any;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+};
+
+export const CounterButton: React.FC<PropsButton> = ({
+  label,
+  children,
+  ...props
+}: PropsButton) => {
   const [count, setCount] = React.useState(0);
   return (
     <div className="counter-button">
       <p>
-        This component is from{" "}
-        <code>
-          ui
-        </code>
+      {label}
+      {children}
       </p>
       <div>
         <button className="counter"
           type="button"
           onClick={() => setCount((c) => c + 1)}
+          {...props}
         >
           Count: {count}
         </button>
--- packages/ui/react-library/src/atoms/input/Input.tsx
+++ packages/ui/react-library/src/atoms/input/Input.tsx
@@ -2,18 +2,27 @@ import React from 'react';
 
 interface PropsInput {
   type?: React.HTMLInputTypeAttribute | undefined;
+  label?: string;
   value?: string;
-  onChange?: React.ChangeEventHandler<HTMLInputElement>;
   className?: string;
   id?: string;
   required?: boolean;
   placeholder?: string;
 }
 
-export const Input: React.FC<PropsInput> = ({ ...props }) => {
+export const Input: React.FC<PropsInput> = ({ id, label, value, placeholder, className, required, ...props }) => {
   return (
     <>
-      <input {...props} />
+      <div className={className}>
+        <p>
+        {label}
+        </p>
+        <div>
+          <input id={id} {...props} placeholder={placeholder} required={required}>
+          {value}
+          </input>
+        </div>
+      </div>
     </>
   );
 };
