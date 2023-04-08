1d17e8abec640344dc99a43e4d58c7d443917023
Kong To <newlight77@gmail.com>
2023-04-08 23:19:58 +0200

    add markdown marshall functions

--- packages/lib/markdown-library/src/marshaller/markdown-marshaller.ts
+++ packages/lib/markdown-library/src/marshaller/markdown-marshaller.ts
@@ -52,30 +52,36 @@ class MarkdownMarshaller {
             case "numbered_list_item": return md.bullet(this.marhsallTextContent(type, block), block.numbered_list_item.number);
             case "quote": return md.quote(this.marhsallTextContent(type, block));
             case "to_do": return md.todo(this.marhsallTextContent(type, block), block.to_do.checked);
-            case "toggle": return "//TODO toggle"
-            case "template": return "//TODO template"
-            case "synced_block": return "//TODO synced_block"
-            case "child_page": return "//TODO child_page"
-            case "child_database": return "//TODO child_database"
             case "equation": return md.codeBlock(block.equation.expression);
             case "code": return md.codeBlock(this.marhsallTextContent(type, block), block[type].language);
-            case "callout": return "//TODO callout"
             case "divider": return md.divider();
-            case "breadcrumb": return "//TODO breadcrumb"
-            case "table_of_contents": return "//TODO table_of_contents"
-            case "column_list": return "//TODO column_list"
-            case "column": return "//TODO column"
-            case "link_to_page": return "//TODO link_to_page"
-            case "table": return "//TODO table"
-            case "table_row": return "//TODO table_row"
-            case "embed": return "//TODO embed"
-            case "bookmark": return "//TODO bookmark"
+
+            case "template": return md.paragraph(this.marhsallTextContent(type, block));
+
+            case "child_page": return this.marhsallChildPage(type, block);
+            case "child_database": return this.marhsallChildDatabase(type, block);
+            case "link_to_page": return this.marhsallLinkToPage(type, block);
+            case "link_preview": return this.marhsallLinkPreview(type, block);
+            case "embed": return this.marhsallEmbed(type, block);
+            case "bookmark": return this.marhsallBookmark(type, block);
+
             case "image": return this.marshallMediaFile(type, block);
             case "video": return this.marshallMediaFile(type, block);
             case "pdf": return this.marshallMediaFile(type, block);
             case "file": return this.marshallMediaFile(type, block);
             case "audio": return this.marshallMediaFile(type, block);
-            case "link_preview": return "//TODO link_preview"
+
+            case "callout": return this.marshallCallout(type, block);
+
+            case "synced_block": return "" // ignore as should not be present in adapted data
+            case "breadcrumb": return "" // no intention to supported here, no content
+            case "table_of_contents": return "" // no intention to supported here, no content
+            case "column_list": return "" // no intention to supported here, no content, only layout
+            case "column": return "" // no intention to supported here, no content, only layout
+
+            case "table": return this.marshallTable(type, block);
+            case "table_row": return "" // rows are content of table, so processed as by table above
+            case "toggle": return this.marshallToggle(type, block);
 
             case "unsupported":
             default: 
@@ -114,6 +120,105 @@ class MarkdownMarshaller {
         return annotatedText;
     }
 
+    private marhsallChildPage(type: string, block: any): string {
+        if ( type !in ["child_page"]) return "error : not a child_page type"
+
+        const blockContent = block[type];
+        const text = blockContent.title;
+        const url = block.id;
+
+        return this.marshallLink(text, url);
+    }
+
+    private marhsallChildDatabase(type: string, block: any): string {
+        if ( type !in ["child_database"]) return "error : not a child_database type"
+
+        const blockContent = block[type];
+        const text = blockContent.title;
+        const url = block.id;
+
+        return this.marshallLink(text, url);
+    }
+
+    private marhsallLinkToPage(type: string, block: any): string {
+        if ( type !in ["link_to_page"]) return "error : not a link_to_page type"
+
+        const blockContent = block[type];
+        const text = blockContent[blockContent.type];
+        const url = text;
+        return this.marshallLink(text, url);
+    }
+
+    private marhsallLinkPreview(type: string, block: any): string {
+        if ( type !in ["link_preview"]) return "error : not a link_preview type"
+
+        const blockContent = block[type];
+        const text = blockContent.url;
+        const url = blockContent.url;
+
+        return this.marshallLink(text, url);
+    }
+
+    private marhsallEmbed(type: string, block: any): string {
+        if ( type !in ["embed"]) return "error : not a embed type"
+
+        const blockContent = block[type];
+        const text = blockContent.caption;
+        const url = block.url;
+
+        return this.marshallLink(text, url);
+    }
+
+    private marhsallBookmark(type: string, block: any): string {
+        if ( type !in ["bookmark"]) return "error : not a bookmark type"
+
+        const blockContent = block[type];
+        const text = blockContent.caption;
+        const url = block.url;
+
+        return this.marshallLink(text, url);
+    }
+
+    private marshallMediaFile(type: string, block: any): string {
+        if ( type !in ["file" , "image", "video", "audio", "pdf"]) return "error : not a valid media file"
+
+        const fileBlock = block[type];
+        const text = fileBlock.caption
+            .map((item: any) => item.plain_text)
+            .join("");
+        const url = fileBlock.type === "external" ? fileBlock.external.url : fileBlock.file.url; 
+        return md.image(text, url);
+    }
+
+    private marshallCallout(type: string, block: any): string {
+        if ( type !in ["callout"]) return "error : not a callout type"
+
+        const text = this.marhsallTextContent(type, block)
+        const icon = block.url;
+
+        return md.callout(text, icon);
+    }
+
+    private marshallTable(type: string, block: any): string {
+        if ( type !in ["callout"]) return "error : not a table type"
+
+        const blockContent = block[type];
+
+        const tableArr: string[][] = blockContent.table || [];
+
+        return md.table(tableArr);
+    }
+
+    private marshallToggle(type: string, block: any): string {
+        if ( type !in ["callout"]) return "error : not a table type"
+
+        const blockContent = block[type];
+        const summary = blockContent.rich_text[0]?.plain_text;
+        const childText = "";
+
+        return md.toggle(summary, childText);
+    }
+
     private annotatePlainText(text: string, annotations: Annotations): string {
         if (text && text.trim() === "") return text;
 
@@ -135,20 +240,9 @@ class MarkdownMarshaller {
     }
 
     private marshallLink(plainText: string, href: string, ) {
-        if (href && href !== "") return md.link(plainText, href);
+        if (plainText && plainText !== "" && href && href !== "") return md.link(plainText, href);
         // logger.info('annotateLink, annotatedText', md.link(plainText, href))
         return plainText;
-    }
-
-    private marshallMediaFile(type: string, block: any): string {
-        if ( type !in ["file" , "image", "video", "audio", "pdf"]) return "error : not a valid media file"
-
-        const fileBlock = block[type];
-        const caption_plain = fileBlock.caption
-            .map((item: any) => item.plain_text)
-            .join("");
-        const url = fileBlock.type === "external" ? fileBlock.external.url : fileBlock.file.url; 
-        return md.image(caption_plain, url);
     }
 
 }
